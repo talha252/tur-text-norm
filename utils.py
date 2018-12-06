c3620fc8832f0523f41052c87f8d0b45e1d10199
Talha Çolaoğlu <talhacolakoglu@gmail.com>
2018-12-07 02:50:04 +0300

    Add write_json to utility functions

--- utils.py
+++ utils.py
@@ -6,11 +6,13 @@ def read_data(filename):
     with open(filename) as fp:
         return json.load(fp)
 
-
+def write_json(filename, data):
+    with open(filename, 'w') as fp:
+        json.dump(data, fp, indent=4, ensure_ascii=False, sort_keys=True)
+        
 def get_twitter_config(path):
     with open(path) as cf:
         return json.load(cf)
-
 
 def connect_database(database, host="localhost", port=27017):
     client = MongoClient(f"mongodb://{host}:{port}")
