45b02673168e26d03c388bd556b6084f92facaa2
Talha Çolakoğlu <talhacolakoglu@gmail.com>
2019-01-22 23:52:53 +0300

    utils: Fix attr_dict constructor

--- turkish_normalization/utils/attr_dict.py
+++ turkish_normalization/utils/attr_dict.py
@@ -5,7 +5,7 @@ class Attrdict(dict):
         super().__init__(*args, **kwargs)
         for k, v in kwargs.items():
             if isinstance(v, dict):
-                self[k] = Config(**v)
+                self[k] = Attrdict(**v)
         
     def __getattr__(self, name):
         return self[name]
