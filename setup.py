8c4918d20aa3cd3b2f76e76e3b0f26703ece339b
Talha Çolakoğlu <talhacolakoglu@gmail.com>
2019-01-22 23:06:26 +0300

    project: Fix dependecies & install requirements

--- setup.py
+++ setup.py
@@ -2,42 +2,54 @@
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
-with open('README.md') as readme_file:
+
+install_requires = [
+    "cython>=0.29.3",
+    "numpy>=1.16.0",
+    "pymongo>=3.7.2",
+    "python-twitter>=3.5",
+    "redis>=3.0.1",
+    "toml>=0.10.0",
+    "unidecode>=1.0.23",
+]
+
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='turkish_normalization',
-    version='0.0.1a',
-    description='A complete text normalizer framework for Turkish language',
+    name="turkish_normalization",
+    version="0.0.1a",
+    description="A complete text normalizer framework for Turkish language",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/talha252/tur-text-norm',
-    author='Talha Çolakoğlu',
-    author_email='talhacolakoglu@gmail.com',
-    license='GPL v3.0+',
+    long_description_content_type="text/markdown",
+    url="https://github.com/talha252/tur-text-norm",
+    author="Talha Çolakoğlu",
+    author_email="talhacolakoglu@gmail.com",
+    license="GPL v3.0+",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: Turkish',
-        'Operating System :: OS Independent',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Linguistic',
-        'Topic :: Utilities'
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: Turkish",
+        "Operating System :: OS Independent",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Utilities",
     ],
-    keywords='turkish text normalization',
+    keywords="turkish text normalization",
     packages=find_packages(include=["turkish_normalization"]),
-    setup_requires=[
-        'setuptools >= 18.0',
-        'cython'
-    ],
-    entry_points = {
-        'console_scripts': [
+    install_requires=install_requires,
+    setup_requires=["setuptools >= 18.0", "cython"],
+    python_requires=">=3.6,<4.0",
+    entry_points={
+        "console_scripts": [
             "tweet-grabber=turkish_normalization.twitter_scrape.tweet_grabber:main"
-        ],
+        ]
     },
-    ext_modules=cythonize("turkish_normalization/turkish_levenshtein/weighted_levenshtein/clev.pyx")
+    ext_modules=cythonize(
+        "turkish_normalization/turkish_levenshtein/weighted_levenshtein/clev.pyx"
+    ),
 )
