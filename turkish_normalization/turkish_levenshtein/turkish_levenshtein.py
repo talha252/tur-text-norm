f5a3b9b5e73f0cdc8f94f5a5aaa0abdb126704d0
Talha Çolakoğlu <talhacolakoglu@gmail.com>
2019-01-27 21:50:05 +0300

    tr-levenshtein: Change encoding func. from lev-turkish to modified ascii
    lev-turkish encoding couldn't met the speed expectations. It runs 5 times slower ascii encoding in python. I've changed the default encoding to ascii. However, ascii can't encode Turkish spesific characters. For that, I've translated the turkish spesific chars to punctuation marks that are not needed in the project. With this, now encoding runs 3 times faster.

--- turkish_normalization/turkish_levenshtein/turkish_levenshtein.py
+++ turkish_normalization/turkish_levenshtein/turkish_levenshtein.py
@@ -1,18 +1,35 @@
 import numpy as np
 from . import lev_encoding
 from .weighted_levenshtein import dam_lev
+from turkish_normalization.utils.turkish_sanitize import turkish_spesific_chars as tsc
 # TODO: add docstrings
 # TODO: add type annotations
 
 # TURKISH_ENCODING = "iso-8859-9"
-TURKISH_ENCODING = "lev-turkish"
+# TURKISH_ENCODING = "lev-turkish"
+TURKISH_ENCODING = "ascii"
+
+START_POS = 64 - len(tsc)
+assert START_POS > 32 # printable character begin
+ascii_translate_table = {ord(t_c):chr(a_c) for a_c, t_c in enumerate(tsc, start=START_POS)}
 
 def w_ord(src_char):
-    return int.from_bytes(src_char.encode(TURKISH_ENCODING), "big")
-
+    return ord(tur_enc(src_char))
+    # try:
+    #     return int.from_bytes(src_char.encode(TURKISH_ENCODING), "big")
+    # except:
+    #     return 0
 
 def tur_enc(src_str):
-    return src_str.encode(TURKISH_ENCODING)
+    try:
+        res = src_str.encode(TURKISH_ENCODING)
+    except UnicodeEncodeError:
+        src_str = src_str.translate(ascii_translate_table)
+        # try:
+        res = src_str.encode(TURKISH_ENCODING)
+        # except UnicodeEncodeError:
+        #     res = b""
+    return res
 
 
 def initiliaze_costs(chars, costs):
