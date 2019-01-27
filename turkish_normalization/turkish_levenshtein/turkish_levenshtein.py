d3eb389e9b84bc44d67759385e3436ca2d40e362
Talha Çolakoğlu <talhacolakoglu@gmail.com>
2019-01-27 21:55:25 +0300

    tr-levenshtein: Rewrite the turkish_levenshtein module according to change of clev module

--- turkish_normalization/turkish_levenshtein/turkish_levenshtein.py
+++ turkish_normalization/turkish_levenshtein/turkish_levenshtein.py
@@ -1,5 +1,6 @@
 import numpy as np
 from . import lev_encoding
+from . import weighted_levenshtein as wl
 from .weighted_levenshtein import dam_lev
 from turkish_normalization.utils.turkish_sanitize import turkish_spesific_chars as tsc
 # TODO: add docstrings
@@ -31,6 +32,7 @@ def tur_enc(src_str):
         #     res = b""
     return res
 
+set_costs = wl.set_costs
 
 def initiliaze_costs(chars, costs):
     if not isinstance(costs, list):
@@ -53,31 +55,15 @@ def turkish_levenshtein(
     target_word,
     *,
     threshold=None,
-    ignore_repeating_char=True,
-    insert_costs=None,
-    substitute_costs=None,
-    delete_costs=None,
-    delete_adjacent_costs=None,
 ):
-    # TODO: not sure, word counts belong here
-    # dist_list = []
     if threshold is None:
         threshold = np.finfo(np.float64).max
-    if ignore_repeating_char:
-        delete_repeating_costs = np.zeros(256, dtype=np.float64)
 
     for word in source_words:
         dist = dam_lev(
             word,
             target_word,
             threshold=threshold,
-            encoding=TURKISH_ENCODING,
-            insert_costs=insert_costs,
-            substitute_costs=substitute_costs,
-            delete_costs=delete_costs,
-            delete_adjacent_costs=delete_adjacent_costs,
-            delete_repeating_costs=delete_repeating_costs,
         )
         yield (dist, word)
-        # dist_list.append((dist, word))
-    # return dist_list
+
